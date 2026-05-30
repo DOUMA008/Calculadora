d17e03fc53bf6636365f26eb6786f83271c18444
DOUMA008 <hugonunes.vitor11@gmail.com>
2026-05-30 13:17:28 -0300

    Atualização do projeto

--- treino.py
+++ treino.py
@@ -1,2 +1,12 @@
 print('===Cauculadora===')
-soma =
+numero01 = float(input('Digite um número: '))
+numero02 = float(input('Digite outro numero: '))
+
+soma = numero01 + numero02
+sub = numero01 - numero02
+mult = numero01 * numero02
+divi = numero01 / numero02
+print(soma)
+print(sub)
+print(mult)
+print(divi)
