b484b1f123c2263dc2815137fd95827b5352e9b9
DOUMA008 <hugonunes.vitor11@gmail.com>
2026-05-31 23:50:05 -0300

    Atualização do projeto

--- treino.py
+++ treino.py
@@ -6,35 +6,37 @@ while True:
     2-subtrair
     3-multiplicar
     4-dividir
-    5-Sair   
+    x-Sair   
     ''')
 
     opcao = input('Escolha uma opção:')
-    if opcao == '1':
+
+    if opcao == 'x':
+        print('Encerrando...')
+        break
+
+    if opcao in ('1', '2', '3', '4'):
         n1 = float(input('Digite o primeiro numero: '))
         n2 = float(input('Digite o segundo numero: '))
+
+    if opcao == '1':
+
         print('O resultado é:',n1 + n2)
 
     elif opcao == '2':
-        n1 = float(input('Digite o primeiro numero: '))
-        n2 = float(input('Digite o segundo numero: '))
+
         print('O resultado é:',n1 - n2)
 
     elif opcao == '3':
-        n1 = float(input('Digite o primeiro numero: '))
-        n2 = float(input('Digite o segundo numero: '))
+
         print('O resultado é:',n1 * n2)
 
     elif opcao == '4':
-        n1 = float(input('Digite o primeiro numero: '))
-        n2 = float(input('Digite o segundo numero: '))
+
         if n2 == 0:
             print('Error...')
         else:
             print('O resultado é:', n1 / n2)
 
-    elif opcao == '5':
-        print('Encerrando...')
-        break
     else:
         print('Error')
